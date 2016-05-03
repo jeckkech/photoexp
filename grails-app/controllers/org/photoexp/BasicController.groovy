53283f1933f6d12f954725b6a538e46e7adeef95
Maxim Rossiytsev <max@rossiytsev.com>
2016-05-03 21:18:05 +0300

    GridFS + user->images connection + base64 thumbnails + image service

--- grails-app/controllers/org/photoexp/BasicController.groovy
+++ grails-app/controllers/org/photoexp/BasicController.groovy
@@ -7,6 +7,8 @@ import com.mongodb.gridfs.GridFSDBFile
 import grails.transaction.Transactional
 import org.photoexp.entity.data.Image
 
+import javax.imageio.ImageIO
+
 /**
  * Created by Max on 20.02.2016.
  */
@@ -19,15 +21,15 @@ class BasicController {
         def user = springSecurityService.currentUser;
 
 
-//        MongoClient client = new MongoClient();
-//        DB db = client.getDB("mongo");
-//
-//        GridFS gridFSPhoto = new GridFS(db, "photo");
-//        for(Image image : user.getImages()){
-//            System.out.println(image.name);
-//            GridFSDBFile imageFile = gridFSPhoto.findOne(image.name);
-//            System.out.println(imageFile)
-//        }
+        MongoClient client = new MongoClient();
+        DB db = client.getDB("mongo");
+
+        GridFS gridFSPhoto = new GridFS(db, "photo");
+        for(Image image : user.getImages()){
+            System.out.println(image.name);
+            GridFSDBFile imageFile = gridFSPhoto.findOne(image.name);
+            System.out.println(ImageIO.read(imageFile.getInputStream()));
+        }
 
         return [user: user];
     }
